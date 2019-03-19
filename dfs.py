95ff571edc91e14d335157916a0c59ba220d704b
Chris <chris@Chriss-MacBook-Pro.local>
2019-03-19 22:57:48 +0800

    graph dfs search

---
+++ dfs.py
@@ -0,0 +1,29 @@
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+# iterative way
+
+def dfs1(graph, start):
+    visited, stack = set(), [start]
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            visited.add(node)
+            stack.extend(graph[node] - visited)
+    return visited
+
+def dfs2(graph, start, visited=None):
+    if visited == None:
+        visited = set()
+    visited.add(start)
+    for node in graph[start] - visited:
+        dfs2(graph, node, visited)
+    return visited
+
+
+
+print(dfs2(graph, 'A'))
