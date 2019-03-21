e6b2d7d6d2283c09ae4a938f2e66555fbd9f9efa
Chris <chris@Chriss-MacBook-Pro.local>
2019-03-21 17:41:58 +0800

    add comment

--- subSets.java
+++ subSets.java
@@ -7,16 +7,19 @@ public class subSets {
         dfs(num, res, new ArrayList<>(), 0);
         return res;
     }
-    private static void dfs(int[] num, List<List<Integer>> res,List<Integer> path, int index) {
-        res.add(new ArrayList<>(path));
+    //backtracking method: think when and where it stop
+    private static void dfs(int[] num, List<List<Integer>> res, List<Integer> path, int index) {
+        if(index <= num.length) {
+            res.add(new ArrayList<>(path));
+        }   
         for(int i=index; i<num.length; i++) {
-            path.add(num[i]);
+            path.add(num[i]);   
             dfs(num, res, path, i+1);
             path.remove(path.size()-1);
         }
     }
-    public static void main(String[] args) {
-        int[] testArr = new int[] {1,2,3,4};
+    public static void main (String[] args) {
+        int[] testArr = new int[] {1,2,3,};
         System.out.print(subArr(testArr));
     }
 }
