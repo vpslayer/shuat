c431897492e8a8ab34cfb4988184a8f48afff38c
Chris <chris@Chriss-MacBook-Pro.local>
2019-03-27 22:32:01 +0800

    initial commit

---
+++ CombinationSum3.java
@@ -0,0 +1,27 @@
+import java.util.List;
+import java.util.ArrayList;
+
+// tag: backtracking
+
+public class CombinationSum3 {
+    public static List<List<Integer>> combination (int k, int n) {
+        List<List<Integer>> result = new ArrayList<>();
+        combination(result, new ArrayList<>(), k, n, 1);
+        return result;
+    }
+    private static void combination(List<List<Integer>> res, ArrayList<Integer> temp, int len, int target, int start) {
+        if(temp.size() == len && target == 0) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        // prune branches in for loop
+        for(int i=start; i<10 && temp.size()<len && i<=target; i++) {
+            temp.add(i);
+            combination(res, temp, len, target-i, i+1);  // i+1 -> avoid duplicates in recursion
+            temp.remove(temp.size()-1);
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println(combination(3, 7));
+    }
+}
