402a5a3eec95617cc9e75197a61b4f1933aeb148
Chris <chris@Chriss-MacBook-Pro.local>
2019-04-13 23:08:23 +0800

    add getter method

--- tree/Node.java
+++ tree/Node.java
@@ -1,9 +1,16 @@
-class Node {
+public class Node {
     int val;
     Node left, right;
+
     Node(int x) { 
         this.val = x;
         this.left = null;
         this.right = null;
     }
+
+    public Node getLeft() {return this.left;}
+    
+        public Node getRight() {return this.right;}
+    
+        public String getText() {return Integer.toString(this.val);}
 }
