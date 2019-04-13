e0781c122aa25d76da2f4b6114578b33b4282dce
Chris <chris@Chriss-MacBook-Pro.local>
2019-04-13 23:08:55 +0800

    add new method

--- tree/Node.java
+++ tree/Node.java
@@ -1,4 +1,6 @@
-public class Node {
+class Node {
+    // 如果属性前面都改成private，类的实例就不能访问了
+    // 但是最好用private，使用getter和setter来访问和修改属性值
     int val;
     Node left, right;
 
@@ -7,10 +9,21 @@ public class Node {
         this.left = null;
         this.right = null;
     }
-
-    public Node getLeft() {return this.left;}
+    Node getLeft() {
+        return this.left;
+    }
     
-        public Node getRight() {return this.right;}
+    Node getRight() {
+        return this.right;
+    }
     
-        public String getText() {return Integer.toString(this.val);}
+    String getText() {
+        return Integer.toString(this.val);
+    }
+    public static void main(String[] args) {
+        Node tree = new Node(5);
+        tree.left = new Node(4);
+        tree.right = new Node(8);
+        System.out.println(tree.getText());
+    }
 }
